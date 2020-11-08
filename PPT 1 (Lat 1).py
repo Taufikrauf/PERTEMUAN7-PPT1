28cd83dc618ea3464c63579ccd6a60feb6d13892
Taufikrauf <72906441+Taufikrauf@users.noreply.github.com>
2020-11-08 13:45:15 +0700

    Update PPT 1 (Lat 1).py

--- PPT 1 (Lat 1).py
+++ PPT 1 (Lat 1).py
@@ -2,7 +2,7 @@ baris = 10
 kolom = baris
 
 for bar in range(baris):
-    for col in range(kolom):
+    for col1 in range(kolom):
         tab = bar+col
         print("{0:>5}".format(tab), end='')
     print()
